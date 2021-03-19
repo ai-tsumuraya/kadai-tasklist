548b7f3d8e1f0d472f92723979056dfb8fb8a170
ai.tsumuraya <aitsumuraya@gmail.com>
2021-03-19 01:13:02 +0000

    fixed create account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, except: [:new]
+  before_action :require_user_logged_in, except: [:new, :create]
   
   def index
   end
